b179a804f6954522534633c1da419cef1e3ad347
Bruno Moacir <brunopmoacir129@gmail.com>
2026-03-17 15:13:37 -0300

    bolha implementado

---
+++ ordenamentos/bolha/bolha.java
@@ -0,0 +1,17 @@
+public class bolha{
+	public static void main(String [] args){
+		int [] vtr = {3,6,7,4,5,6};
+		int n = 6;
+
+		for(int i = n - 1; i > 0; i --){
+			for(int j = 0; j < i; j++){
+				if(vet[j] > vet[j + 1]{
+					int tmp = vet[j];
+					vet[j] = vet[j + 1];
+					vet[j + 1] = tmp;
+				}		
+			}
+		}
+	}
+}
+
