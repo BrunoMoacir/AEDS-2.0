30a7be686ca1b3fca9b4f913441a1e45317d5d74
Bruno Moacir <brunopmoacir129@gmail.com>
2026-03-18 17:56:55 -0300

    comeco tp

---
+++ tp-1/questao1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class questao1{
+    public static void main(String[] args) {
+        Scanner sc = new scanner (System.in);
+
+        while(sc.hasNextLine())
+    }
+}
