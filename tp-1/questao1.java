fa62cc9d5e6f609fbaa7b29c892d9d0768de9ec4
Bruno Moacir <brunopmoacir129@gmail.com>
2026-03-22 12:43:53 -0300

    q1 tp

--- tp-1/questao1.java
+++ tp-1/questao1.java
@@ -1,9 +1,26 @@
-import java.util.Scanner;
+public class questao1 {
 
-public class questao1{
     public static void main(String[] args) {
-        Scanner sc = new scanner (System.in);
+        String linha = MyIO.readLine();
+        while (linha != null && linha.length() > 0){// enquanto tiver coisa para ler
+            // chamada funcao
+            String mensagemCriptografada = cifrar(linha);
+            
+            MyIO.println(mensagemCriptografada);
 
-        while(sc.hasNextLine())
+            linha = MyIO.readLine();
+        }
+    }
+
+    //funcao para cifrar
+    public static String cifrar(String texto) {
+        String resultado = "";
+
+        for (int i = 0; i < texto.length(); i++) {
+            char novaLetra = (char) (texto.charAt(i) + 3);// pego a letra atual e somo 3 posicoes
+            resultado += novaLetra;// concateno ate dar a palavra completa
+        }
+
+        return resultado;
     }
 }
