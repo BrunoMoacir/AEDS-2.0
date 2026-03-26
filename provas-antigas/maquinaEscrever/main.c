d82f3153a5cf903e78392018de1d067b620e3a5c
Bruno Moacir <brunopmoacir129@gmail.com>
2026-03-25 21:27:33 -0300

    maquina escrever em C

--- provas-antigas/maquinaEscrever/main.c
+++ provas-antigas/maquinaEscrever/main.c
@@ -2,8 +2,28 @@
 #include <stdlib.h>
 
 int main(){
-	int numeroDefeituoso;
-	while(scanf("%d", &numeroDefeituoso) != 0){
-		:q
+	char d,n[100];
+
+	while(scanf(" %c,%s"m %d, n) && (d != 0)){
+		char resultado[100];
+		int j = 0;
+
+		for(int i = 0;n[i] != '\0'; i++){
+			if(n[i] != d){
+				resultado[j ++] = n[i];
+			}
+		}
+		resultado[j] = '\0';
+		int inicio = 0;
+		while(inicio < j && resultado[inicio] == '0'){
+			inicio ++;
+		}
+
+		if(inicio == j){
+			printg("0\n");
+		} else{
+			printf("%s\n", &resultado[inicio]);
+		}
 	}
+	return 0;
 }
