46e3023323924380515579e722038ddeb946bb2e
Bruno Moacir <brunopmoacir129@gmail.com>
2026-04-13 23:28:15 -0300

    questao 9 100%

--- tp-1/questao9.c
+++ tp-1/questao9.c
@@ -7,28 +7,28 @@ int tamanho(char str[])
     {
         count++;
     }
-    return count;
+    return count;// retorno o tamanho
 }
 
 void cifraRecursivo(char str[], int i)
 {
-    if (str[i] == '\0')
+    if (str[i] == '\0')// quando a string chegar no fim acaba
     {
         return;
     }
 
-    str[i] = str[i] + 3;
+    str[i] = str[i] + 3;// cifro ela
 
-    cifraRecursivo(str, i + 1);
+    cifraRecursivo(str, i + 1);// chamo a funcao recursivamente para continuar no proximo caractere
 }
 
 int main()
 {
     char linha[1000];
 
-    while (fgets(linha, sizeof(linha), stdin) != NULL)
+    while (fgets(linha, sizeof(linha), stdin) != NULL)// enquanto tiver linha para ler
     {
-        // remover o '\n' manualmente
+        // remover o \n 
         int i = 0;
         while (linha[i] != '\0')
         {
@@ -40,12 +40,12 @@ int main()
             i++;
         }
 
-        if (linha[0] == 'F' && linha[1] == 'I' && linha[2] == 'M')
+        if (linha[0] == 'F' && linha[1] == 'I' && linha[2] == 'M')// se for FIM, acaba as leituras
         {
             break;
         }
 
-        cifraRecursivo(linha, 0);
+        cifraRecursivo(linha, 0);// chamo a funcao passando a string e o numero de i (comeco) 0
         printf("%s\n", linha);
     }
 
