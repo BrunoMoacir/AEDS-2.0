607174f6624ab0174ea5275633ba6989d231f699
Bruno Moacir <brunopmoacir129@gmail.com>
2026-04-18 12:00:00 -0300

    preparacao tp 2

---
+++ MyIO.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.nio.charset.*;
+
+public class MyIO {
+
+    private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in, Charset.forName("ISO-8859-1")));
+    private static String charset = "ISO-8859-1";
+
+    public static void setCharset(String charset_) {
+        charset = charset_;
+        in = new BufferedReader(new InputStreamReader(System.in, Charset.forName(charset)));
+    }
+
+    public static void print(String x) {
+        try {
+            PrintStream out = new PrintStream(System.out, true, charset);
+            out.print(x);
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Erro: charset invalido");
+        }
+    }
+
+    public static void println(String x) {
+        try {
+            PrintStream out = new PrintStream(System.out, true, charset);
+            out.println(x);
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Erro: charset invalido");
+        }
+    }
+
+    public static String readLine() {
+        String s = "";
+        char tmp;
+        try {
+            do {
+                tmp = (char) in.read();
+                if (tmp != '\n' && tmp != 13) {
+                    s += tmp;
+                }
+            } while (tmp != '\n');
+        } catch (IOException ioe) {
+            System.out.println("lerPalavra: " + ioe.getMessage());
+        }
+        return s;
+    }
+}
