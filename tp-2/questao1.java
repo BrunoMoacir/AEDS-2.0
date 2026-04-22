5a31e117831d6a07ac68e83d944610c188a9fe46
Bruno Moacir <brunopmoacir129@gmail.com>
2026-04-22 14:40:23 -0300

    comeco questao 1

--- tp-2/questao1.java
+++ tp-2/questao1.java
@@ -1,5 +1,181 @@
-package tp-2;
+import java.io.File;
+import java.util.Scanner;
 
-public class questao1 {
+class Util{// base para todo o tp2
+    public static int paraInt(String s){
+        int res = 0;
+        for(int i = 0; i < s.length(); i++){
+            res = res * 10 + (s.charAt(i) - '0');// converto o caractere 5 no numero inteiro 5 usando a tabela ascii, 
+        }
+        return res;// retorno o inteiro 
+    }
+    public static double paraDouble(String s){
+        double res = 0;
+        double div = 1;
+        boolean dpsPonto = false;//ponto para saber se ja passei do ponto decimal
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '.'){
+                dpsPonto = true;
+            }else{
+                if(!dpsPonto){// se ainda for a parte inteira, continuo
+                    res = res * 10 + (c - '0');
+                } else{// se for a parte fracionaria divido por 10 -> 100 ...
+                    div *= 10;
+                    res = res + (double)(c - '0') / div;
+                }
+            }
+        }
+        return res;// retorno o double
+    }
+    public static String[] extrairCampos(String s, char sep){
+        int cont = 1;// conto quantas palavras vao sair
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == sep){
+                cont ++;
+            }
+        }
+
+        String[] partes = new String[cont];// crio o array com o tamanho certo
+        int idx = 0;// ponteiro de onde no array estou preenchendo
+        String atual = "";// palavra que to montando letra por letra
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == sep){
+                partes[idx++]= atual;// quando acho o separador salvo a palavra no array
+                atual = "";// limpo a string para comecar a montar a nova palavra
+            }else{
+                atual += c;// se nao for o separador continuo montando a palavra
+            }
+        }
+        partes[idx] = atual;// salvo o pedaco da palavra q sobrou no final
+        return partes;
+    }
+}
+
+// CLASSE DE ENTIDADE (data, hora, restaurante)
+class Data{
+    private int ano,mes,dia;
+
+    public Data(){}
+    public int getAno(){
+        return ano;
+    }
+    public void setAno(int ano){
+        this.ano = ano;
+    }
+    public int getMes(){
+        return mes;
+    }
+    public void setMes(int mes){
+        this.mes = mes;
+    }
+    public int getDia(){
+        return dia;
+    }
+    public void setDia(int dia){
+        this.dia = dia;
+    }
+
+    public static Data parseData(String s){
+        Data d = new Data();
+
+        String[] p = Util.extrairCampos(s, "-");// uso meu util para cortar a data no formato certo
+
+        // uso o util para transformar pedacos de texto em numeros
+        d.setAno(Util.paraInt(p[0]));
+        d.setMes(Util.paraInt(p[1]));
+        d.setDia(Util.paraInt(p[2]));
+        return d;
+    }
+
+    public String formatar(){
+        return String.format("%02d/%02d/%04d", dia,mes,ano);// preencho com 0 a esqueda o que for preciso
+    }
+}
+
+class Hora{
+    private int hora,minuto;
+
+    public Hora(){}
+
+    public int getHora(){
+        return hora;
+    }
+    public void setHora(int hora){
+        this.hora = hora;
+    }
+    public int getMinuto(){
+        return minuto;
+    }
+    public void setMinuto(int minuto){
+        this.minuto = minuto;
+    }
+
+    public static Hora parseHora(String s){
+        Hora h = new Hora();
+
+        String[] p = Util.extrairCampos(s, ':');// corto a string no formato hh:mm com os dois pontos
+        h.setHora(Util.paraInt(p[0]));
+        h.setMinuto(Util.paraInt(p[1]));
+        return h;
+    }
+
+    public String formatar(){
+        return String.format("%02d:%02d", hora,minuto);
+    }
+}
+
+class Restaurante{
+    private int id,capacidade,faixaPreco;
+    private String nome, cidade;
+    private double avaliacao;
+    private String[] tiposCozinha;
+    private Hora hAb, hFe;
+    private Data dAb;
+    private boolean aberto;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setCidade(String cidade){
+        this.cidade = cidade;
+    }
+    public void setCapacidade(int capacidade){
+        this.capacidade = capacidade;
+    }
+    public void setAvaliacao(double Avaliacao){
+        this.avaliacao = avaliacao;
+    }
+    public void setTiposCozinha(String[] tc){
+        this.tiposCozinha = tc;
+    }
+    public void setFaixaPreco(int fp){
+        this.faixaPreco = fp;
+    }
+    public void setHAb(hora h){
+        this.hAb = h;
+    }
+    public void setHFe(hora h){
+        this.hFe = h;
+    }
+    public void setDAb(Data d){
+        this.dAb = d;
+    }
+    public void setAberto(boolean a){
+        this.aberto = a;
+    }
+
     
 }
