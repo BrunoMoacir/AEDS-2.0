65480e9aed52d64bbf70fca1024acb9a44fb490d
Bruno Moacir <brunopmoacir129@gmail.com>
2026-04-29 11:57:57 -0300

    pesquisar arvore bin

--- Arvore-Binaria/main.java
+++ Arvore-Binaria/main.java
@@ -1,4 +1,7 @@
 class No{
+    public No(int x) {
+        //TODO Auto-generated constructor stub
+    }
     public int elemento;
     No esq;
     No dir;
@@ -19,5 +22,22 @@ class ArvoreBinaria{
             throw new Exception("erro");
         }
         return i;
+    }
+
+    boolean pesquisar(int x) {
+        return pesquisar(x,raiz);
+    }
+    boolean pesquisar(int x, No i){
+        boolean resp;
+        if(i == null){
+            resp = false;
+        }else if(x == i.elemento){
+            resp = true;
+        }else if(x < i.elemento){
+            resp = pesquisar(x,i.esq);
+        }else{
+            resp = pesquisar(x,i.dir);
+        }
+        return resp;
     }
 }
