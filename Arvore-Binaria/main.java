c57c01983fe4befc991ebbf342786697406cb81b
Bruno Moacir <brunopmoacir129@gmail.com>
2026-05-11 20:24:15 -0300

    remocao arvore

--- Arvore-Binaria/main.java
+++ Arvore-Binaria/main.java
@@ -41,3 +41,33 @@ class ArvoreBinaria{
         return resp;
     }
 }
+
+// REMOCAO
+void remover(int x) throws Exception{
+    return remover(x,raiz);
+}
+No remover(int x, No i) throws Exception{
+    if(i == null){
+        throw new Exception("Erro");
+    }else if(x < i.elemento){
+        return (x,i.esq);
+    }else if(x > i.elemento){
+        return (x,i.dir);
+    }else if(i.dir == null){// caso 1 folha ou caso 2a so filho esq
+        i = i.esq;
+    }else if(i.esq == null){// caso 2b so filho dir
+        i = i.dir;
+    }else{// caso 3 -> dois filhos
+        i.esq = maiorEsq(i,i.esq);
+    }
+    return i;
+}
+No maiorEsq(No i, No j){
+    if(j.dir == null){// j e o maior, nao tem filhos dir
+        i.elemento = j.elemento;//copia para o  o deletado
+        j = j.esq;// removo j
+    }else{
+        j.dir = maiorEsq(i,j.dir);// continuo buscando a direita
+    }
+    return j;
+}
