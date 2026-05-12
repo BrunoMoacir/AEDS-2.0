6d2bca8320bcc701bc8d913c921d7a5618671d14
Bruno Moacir <brunopmoacir129@gmail.com>
2026-05-12 20:52:08 -0300

    revisao #11

--- exercicios/revisao.c
+++ exercicios/revisao.c
@@ -53,3 +53,58 @@ void inverterLista(){
     }
 }
 
+// contar o tamanho da lista dupla circular
+int tamanhoLista(CelulaDupla* primeiro){
+    CelulaDupla* i = primeiro->prox;
+    int count = 0;
+    while(i != primeiro){
+        count ++;
+        i = i->prox;
+    }
+    return count;
+}
+
+// buscar elemento na lista dupla circular com cabeça
+CelulaDupla* buscarElemento(CelulaDUpla* primeiro, int x){
+    CelulaDupla* i = primeiro->prox;
+    while(i != primeiro){
+        if(i->elemento == x){
+            return i;
+        }
+        i = i->prox;
+    }
+    return NULL;
+}
+
+// remover a ultima coluna da matriz flexivel
+void removerUltimaColuna(Matriz *m){
+    Celula* linha = m->inicio;
+    while(linha != null){
+        Celula* col = linha;
+        while(col->dir != null){
+            col = col->dir;// posiciono o ponteiro na coluna na ultima coluna
+        }
+        if(col->esq != null){
+            col->esq->dir = NULL;
+        }
+        Celula* prox = linha->inf;
+        free(col);
+        linha=prox;
+    }
+}
+
+// busca na arvore em c
+No* buscar(No* i, int x){
+    if(raiz == null){
+        return NULL;
+    }
+    if(raiz->numero == val){
+        return raiz;
+    }
+    if(x < raiz->numero){
+        return buscar(raiz->esq, x);
+    }
+    return buscar(raiz->dir,x);
+}
+
+// inserir na arvore em c
