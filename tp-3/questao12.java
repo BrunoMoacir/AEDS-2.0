25fa919db69433d57189fd4d6f7e7355413eb4ed
Bruno Moacir <brunopmoacir129@gmail.com>
2026-05-16 17:07:35 -0300

    correcao q12 tp3

---
+++ tp-3/questao12.java
@@ -0,0 +1,436 @@
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+class Util {// base para todo o tp2
+    public static int paraInt(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            res = res * 10 + (s.charAt(i) - '0');// converto o caractere 5 no numero inteiro 5 usando a tabela ascii,
+        }
+        return res;// retorno o inteiro
+    }
+
+    public static double paraDouble(String s) {
+        double res = 0;
+        double div = 1;
+        boolean dpsPonto = false;// ponto para saber se ja passei do ponto decimal
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '.') {
+                dpsPonto = true;
+            } else {
+                if (!dpsPonto) {// se ainda for a parte inteira, continuo
+                    res = res * 10 + (c - '0');
+                } else {// se for a parte fracionaria divido por 10 -> 100 ...
+                    div *= 10;
+                    res = res + (double) (c - '0') / div;
+                }
+            }
+        }
+        return res;// retorno o double
+    }
+
+    public static String[] extrairCampos(String s, char sep) {
+        int cont = 1;// conto quantas palavras vao sair
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == sep) {
+                cont++;
+            }
+        }
+
+        String[] partes = new String[cont];// crio o array com o tamanho certo
+        int idx = 0;// ponteiro de onde no array estou preenchendo
+        String atual = "";// palavra que to montando letra por letra
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == sep) {
+                partes[idx++] = atual;// quando acho o separador salvo a palavra no array
+                atual = "";// limpo a string para comecar a montar a nova palavra
+            } else {
+                atual += c;// se nao for o separador continuo montando a palavra
+            }
+        }
+        partes[idx] = atual;// salvo o pedaco da palavra q sobrou no final
+        return partes;
+    }
+}
+
+// CLASSE DE ENTIDADE (data, hora, restaurante)
+class Data {
+    private int ano, mes, dia;
+
+    public Data() {
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public static Data parseData(String s) {
+        Data d = new Data();
+
+        String[] p = Util.extrairCampos(s, '-');// uso meu util para cortar a data no formato certo
+
+        // uso o util para transformar pedacos de texto em numeros
+        d.setAno(Util.paraInt(p[0]));
+        d.setMes(Util.paraInt(p[1]));
+        d.setDia(Util.paraInt(p[2]));
+        return d;
+    }
+
+    public String formatar() {
+        return String.format("%02d/%02d/%04d", dia, mes, ano);// preencho com 0 a esqueda o que for preciso
+    }
+}
+
+class Hora {
+    private int hora, minuto;
+
+    public Hora() {
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+
+    public static Hora parseHora(String s) {
+        Hora h = new Hora();
+
+        String[] p = Util.extrairCampos(s, ':');// corto a string no formato hh:mm com os dois pontos
+        h.setHora(Util.paraInt(p[0]));
+        h.setMinuto(Util.paraInt(p[1]));
+        return h;
+    }
+
+    public String formatar() {
+        return String.format("%02d:%02d", hora, minuto);
+    }
+}
+
+class Restaurante {
+    private int id, capacidade, faixaPreco;
+    private String nome, cidade;
+    private double avaliacao;
+    private String[] tiposCozinha;
+    private Hora hAb, hFe;
+    private Data dAb;
+    private boolean aberto;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public void setTiposCozinha(String[] tc) {
+        this.tiposCozinha = tc;
+    }
+
+    public void setFaixaPreco(int fp) {
+        this.faixaPreco = fp;
+    }
+
+    public void setHAb(Hora h) {
+        this.hAb = h;
+    }
+
+    public void setHFe(Hora h) {
+        this.hFe = h;
+    }
+
+    public void setDAb(Data d) {
+        this.dAb = d;
+    }
+
+    public void setAberto(boolean a) {
+        this.aberto = a;
+    }
+
+    public static Restaurante parseRestaurante(String s) {
+        Restaurante r = new Restaurante();
+
+        String[] p = Util.extrairCampos(s, ',');// corto a linha do csv pelas virgulas
+
+        r.setId(Util.paraInt(p[0]));
+        r.setNome(p[1]);
+        r.setCidade(p[2]);
+        r.setCapacidade(Util.paraInt(p[3]));
+        r.setAvaliacao(Util.paraDouble(p[4]));
+
+        r.setTiposCozinha(Util.extrairCampos(p[5], ';'));// corto pelo ponto e virgula
+
+        r.setFaixaPreco(p[6].length());// pego o tamanho da string(ja e a faixa d preco)
+
+        String[] h = Util.extrairCampos(p[7], '-');// o bloco de horario e separado pelo traco
+        r.setHAb(Hora.parseHora(h[0]));
+        r.setHFe(Hora.parseHora(h[1]));
+
+        r.setDAb(Data.parseData(p[8]));
+
+        if (p[9].compareTo("true") == 0) {
+            r.setAberto(true);
+        } else {
+            r.setAberto(false);
+        }
+        return r;
+    }
+
+    public String formatar() {
+        // monto o array de cozinhas
+        String tc = "[";
+        for (int i = 0; i < tiposCozinha.length; i++) {
+            tc += tiposCozinha[i];
+            if (i < tiposCozinha.length - 1) {// so coloco virgula se nao for o ultimo
+                tc += ",";
+            }
+        }
+        tc += "]";
+
+        // monto a faixa de preco e concateno o $
+        String fp = "";
+        for (int i = 0; i < faixaPreco; i++) {
+            fp += "$";
+        }
+
+        String abStr;// transformo o boolean em palavra
+        if (aberto) {
+            abStr = "true";
+        } else {
+            abStr = "false";
+        }
+
+        return String.format("[%d ## %s ## %s ## %d ## %s ## %s ## %s ## %s-%s ## %s ## %s]", id, nome, cidade,
+                capacidade, "" + avaliacao, tc, fp, hAb.formatar(), hFe.formatar(), dAb.formatar(), abStr);
+    }
+}
+
+// COLECAO RESTAURANTES
+class ColecaoRestaurantes {
+    private int n;// contador de restaurantes
+    private Restaurante[] lista;
+
+    public ColecaoRestaurantes() {// construtor
+        n = 0;
+        lista = new Restaurante[1000];// coloquei limite alto, qqr coisa melhoro dps
+    }
+
+    public int getTamanho() {
+        return n;
+    }
+
+    public Restaurante[] getRestaurantes() {
+        return lista;
+    }
+
+    public void lerCsv(String path) throws Exception {
+        Scanner sc = new Scanner(new File(path));
+
+        if (sc.hasNextLine())
+            sc.nextLine();// pulo o cabecalho da primeira linha
+
+        while (sc.hasNextLine()) {
+            String linha = sc.nextLine();
+            if (linha.length() > 0) {
+                String limpa = "";// limpeza para tirar o \r se preciso (enter)
+                for (int i = 0; i < linha.length(); i++) {
+                    if (linha.charAt(i) != '\r') {
+                        limpa += linha.charAt(i);
+                    }
+                }
+                lista[n++] = Restaurante.parseRestaurante(limpa);// salvo o restaurante no espaco atual e avanco o
+                                                                 // ponteiro
+            }
+        }
+        sc.close();
+    }
+}
+
+class No {
+    public Restaurante elemento;
+    public No esq, dir;
+
+    public No(Restaurante elemento) {// ponteiros esq e dir
+        this.elemento = elemento;
+        this.esq = this.dir = null;
+    }
+}
+
+class ArvoreBinaria {
+    private No raiz;
+
+    public ArvoreBinaria() {
+        raiz = null;
+    }
+
+    public void inserir(Restaurante x) {
+        raiz = inserir(x, raiz);
+    }
+
+    private No inserir(Restaurante x, No i) {
+        if (i == null) {
+            i = new No(x);
+        } else if (x.getNome().compareTo(i.elemento.getNome()) < 0) {
+            i.esq = inserir(x, i.esq); // vou p esquerda
+        } else if (x.getNome().compareTo(i.elemento.getNome()) > 0) {
+            i.dir = inserir(x, i.dir); // vou p direita
+        } else {
+            // corrigir depois se der erro
+        }
+        return i;
+    }
+
+    public void pesquisar(String nome) {
+        System.out.print("raiz ");// comeca na raiz
+        pesquisar(nome, raiz);
+    }
+
+    private void pesquisar(String nome, No i) {
+        if (i == null) {
+            System.out.println("NAO"); // nao achei
+        } else {
+            questao12.comparacoes++; // incremento
+            int cmp = nome.compareTo(i.elemento.getNome());
+
+            if (cmp == 0) {
+                System.out.println("SIM"); // achei
+            } else if (cmp < 0) {
+                System.out.print("esq ");
+                pesquisar(nome, i.esq); // vou p esquerda
+            } else {
+                System.out.print("dir ");
+                pesquisar(nome, i.dir);// vou p direita
+            }
+        }
+    }
+
+    public void caminharEmOrdem() {// em ordem -> esq raiz dir
+        caminharEmOrdem(raiz);
+    }
+
+    private void caminharEmOrdem(No i) {
+        if (i != null) {
+            caminharEmOrdem(i.esq);
+            System.out.println(i.elemento.formatar());
+            caminharEmOrdem(i.dir);
+        }
+    }
+}
+
+public class questao12 {
+    public static int comparacoes = 0;
+
+    public static Restaurante buscarPorId(ColecaoRestaurantes col, int id) {
+        for (int i = 0; i < col.getTamanho(); i++) {
+            if (col.getRestaurantes()[i].getId() == id) {
+                return col.getRestaurantes()[i];
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws Exception {
+        ColecaoRestaurantes col = new ColecaoRestaurantes();
+        col.lerCsv("/tmp/restaurantes.csv");
+
+        Scanner sc = new Scanner(System.in);
+        ArvoreBinaria arvore = new ArvoreBinaria();
+
+        while (sc.hasNext()) {// insercao na arvore
+            String idBusca = sc.next();
+            if (idBusca.compareTo("FIM") == 0 || idBusca.compareTo("-1") == 0) {
+                break;
+            }
+
+            int id = Util.paraInt(idBusca);
+            Restaurante r = buscarPorId(col, id);
+
+            if (r != null) {
+                arvore.inserir(r);
+            }
+        }
+
+        sc.nextLine();
+
+        long inicioTempo = System.currentTimeMillis();// ligo cronometro
+
+        while (sc.hasNextLine()) {// pesquiso nomes inteiros
+            String nomeBusca = sc.nextLine();
+            if (nomeBusca.compareTo("FIM") == 0) {// enqto for diferente de FIM
+                break;
+            }
+
+            arvore.pesquisar(nomeBusca);// mando p funcao
+        }
+
+        long fimTempo = System.currentTimeMillis();// paro tempo
+        long tempoTotal = fimTempo - inicioTempo;
+
+        arvore.caminharEmOrdem();// imprimo arvore
+
+        FileWriter writer = new FileWriter("885492_arvore_binaria.txt");
+        writer.write("885492\t" + comparacoes + "\t" + tempoTotal + "\n");
+        writer.close();
+    }
+}
