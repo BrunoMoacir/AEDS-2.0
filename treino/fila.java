a50299e2a23de21aafc10f96b8cd0a10a4da0f75
Bruno Moacir <brunopmoacir129@gmail.com>
2026-05-20 19:06:30 -0300

    fila implementacao rapida

--- treino/fila.java
+++ treino/fila.java
@@ -1,2 +1,30 @@
-public class filIwq
+import java.util.Scanner;
+
+public class fila{
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+
+		int [] fila = new int [1000];
+		int inicio = 0;
+		int fim = 0;
+		int tamanho = 0;
+
+		while(sc.hasNext()){
+			String comando = sc.next();
+			if(comando.charAt(0) == 'E'){
+				fila[fim] = sc.nextInt();
+				fim ++;
+				tamanho ++;
+			}else if(comando.charAt(0) == 'S'){
+				inicio ++;
+				tamanho --;
+			}else{
+				for(int i = inicio; i <= fim; i++){
+					System.out.println(fila[i]);
+				}
+			}
+		}
+	}
+}
 
